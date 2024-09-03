483b5f70d19835ac4a59dd339c9bfbde5fe2b085
Mukul Kathait <kathaitmukul0699@gmail.com>
2024-09-03 14:48:57 +0530

    Unused dependency removed

--- src/components/authComponents/LoginComponent.tsx
+++ src/components/authComponents/LoginComponent.tsx
@@ -1,4 +1,4 @@
-import React, { ErrorInfo, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import InputComponent from "../utilities/InputComponent";
 import GoogleLoginComponent from "./GoogleLoginComponent";
